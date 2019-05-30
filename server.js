a9a79013de9ff1b3d74a8c114025a58df172ad09
AngelLinaStarshine <37021515+AngelLinaStarshine@users.noreply.github.com>
2019-05-29 21:27:36 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const app = express();
 const path = require('path');
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/about.html'));
+    res.sendFile(path.join(__dirname + '/about.html'));
 });
 
 app.get('/contact', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/contact.html'));
+    res.sendFile(path.join(__dirname + '/contact.html'));
 });
 
 app.listen(process.env.PORT || 8080);
